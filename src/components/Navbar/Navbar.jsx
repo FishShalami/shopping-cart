fba8056abc677cd616a1550cfbb83f16df39d3b0
FishShalami <fishshalami@gmail.com>
2025-05-31 23:34:26 -0700

    Styled active page in Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,13 +7,24 @@ export default function Navbar({ cartCount }) {
     <nav>
       <ul>
         <li>
-          <NavLink to="/">Home</NavLink>
+          <NavLink
+            to="/"
+            end
+            className={({ isActive }) => (isActive ? styles.active : undefined)}
+          >
+            Home
+          </NavLink>
         </li>
         <li>
-          <NavLink to="/shop">Shop</NavLink>
+          <NavLink
+            to="/shop"
+            className={({ isActive }) => (isActive ? styles.active : undefined)}
+          >
+            Shop
+          </NavLink>
         </li>
         <li>
-          <NavLink to="/cart">
+          <NavLink to="/cart" aria-label="Go to cart">
             🛒
             <span className={styles.badge}>{cartCount}</span>
           </NavLink>
