b3a43958076059caf3edf87bcda714ebc89e25c5
FishShalami <fishshalami@gmail.com>
2025-06-03 00:29:19 -0700

    Adjust styling for products to make uniform image size; start buttons for each prodcut

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,11 +1,18 @@
+import { useState } from "react";
 import styles from "./ProductCard.module.css";
 
 export default function ProductCard({ product, addToCart }) {
+  const [qty, setQty] = useState(0);
+
   return (
     <div className={styles.productCard}>
       <img src={product.image} alt="{product.title}" />
       <h2>{product.title}</h2>
       <h3>${product.price.toFixed(2)}</h3>
+      <button onClick={() => setQty(qty + 1)}>+</button>
+      <button onClick={() => setQty(Math.max(0, qty - 1))}>-</button>
+      <p>{qty}</p>
+      <button onClick={() => addToCart(product, qty)}>Add to cart</button>
     </div>
   );
 }
