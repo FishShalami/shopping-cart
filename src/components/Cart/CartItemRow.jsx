b918f4ada02d5c3535655a9aab76c1b0cf7b36e7
FishShalami <fishshalami@gmail.com>
2025-06-04 00:48:31 -0700

    cart styling updates

--- src/components/Cart/CartItemRow.jsx
+++ src/components/Cart/CartItemRow.jsx
@@ -1,13 +1,16 @@
 import styles from "./CartItemRow.module.css";
 
 export default function CartItemRow({ item }) {
-  const { title, price, qty } = item;
+  const { title, price, qty, image } = item;
   console.log("item", item);
   return (
-    <li className={styles.itemRow}>
-      <span className={styles.title}>{title} - </span>
-      <span className={styles.qty}>{qty} x </span>
-      <span className={styles.subtotal}>${(price * qty).toFixed(2)}</span>
-    </li>
+    <div className={styles.itemRow}>
+      <img src={image} alt={title} />
+      <div className={styles.itemRowInfo}>
+        <span className={styles.title}>{title} - </span>
+        <span className={styles.qty}>{qty} x </span>
+        <span className={styles.subtotal}>${(price * qty).toFixed(2)}</span>
+      </div>
+    </div>
   );
 }
